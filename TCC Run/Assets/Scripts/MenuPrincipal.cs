ef7c5c7003b67eb44b6a1a41e5b1eb4ec54fca4f
bnoleto <bnoleto@users.noreply.github.com>
2017-10-05 03:40:18 -0300

    Adição do menu principal do jogo, adição do .gitignore para impedir o download do binário pelo github.

---
+++ TCC Run/Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Iniciar(){
+		SceneManager.LoadScene ("Main");
+	}
+
+	public void Sair(){
+		Application.Quit ();
+	}
+}
